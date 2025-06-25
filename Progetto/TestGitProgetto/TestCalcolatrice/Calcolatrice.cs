e32569d4e9d69b9e933febfa2070352abd4804b7
MartyGee9698 <martino.ghio.14-2@accademiadigitaleliguria.it>
2025-06-25 15:50:50 +0200

    AAAA

---
+++ Progetto/TestGitProgetto/TestCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TestCalcolatrice
+{
+    internal class Calcolatrice
+    {
+        public Calcolatrice()
+        {
+        }
+
+        internal double somma(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
