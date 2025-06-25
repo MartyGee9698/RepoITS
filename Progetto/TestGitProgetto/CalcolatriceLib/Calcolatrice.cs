cf461c1fc29649e9b01394a735a7988372ae5d44
MartyGee9698 <martino.ghio.14-2@accademiadigitaleliguria.it>
2025-06-25 15:45:26 +0200

    AAA

---
+++ Progetto/TestGitProgetto/CalcolatriceLib/Calcolatrice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcolatriceLib
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice() { }
+        public double somma(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Progetto/TestGitProgetto/TestCalcolatrice/Test1.cs
@@ -0,0 +1,24 @@
+﻿using CalcolatriceLib;
+
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.somma(2, 3);
+            Assert.AreEqual(5, risultato, 0.0001, "La somma di 2 e 3 dovrebbe essere 5");
+        }
+
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            TestCalcolatrice calcolatrice = new TestCalcolatrice();
+            double risultato = calcolatrice.somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3");
+        }
+    }
+}
